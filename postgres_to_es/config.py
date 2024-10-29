511a25bb42446f6b5015c92edcaa2470038e074d
Vitaly D <vitcode.mail@gmail.com>
2024-10-29 16:19:13 +0300

    pydantic_settings added

--- postgres_to_es/config.py
+++ postgres_to_es/config.py
@@ -1,53 +1,83 @@
 import logging
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DSL = {
-    "dbname": os.environ.get("POSTGRES_NAME"),
-    "user": os.environ.get("POSTGRES_USER"),
-    "password": os.environ.get("POSTGRES_PASSWORD"),
-    "host": os.getenv("SQL_HOST"),
-    "port": os.getenv("SQL_PORT"),
-}
-
-ES_HOST = os.environ.get("ELASTICSEARCH_HOST")
-ES_PORT = os.environ.get("ELASTICSEARCH_PORT")
-
-TABLES = (
-    "film_work",
-    "genre",
-    "person",
-    "genre_film_work",
-    "person_film_work",
-)
-
-DB_SCHEMA = {
-    "postgres": "content.",
-}
-
-BATCH_SIZE = 200
-
-SLEEP_TIME = 10
+from pydantic_settings import BaseSettings
 
 format_log = (
     "#%(levelname)-8s [%(asctime)s] - %(filename)s:"
     "%(lineno)d - %(name)s - %(message)s"
 )
 logging.basicConfig(
-    # level=logging.DEBUG,
     level=logging.INFO,
-    # level=logging.ERROR,
     format=format_log,
 )
-
-# file_handler = logging.FileHandler("logs.log")
-# formatter_file = logging.Formatter(fmt=format_log)
-# file_handler.setFormatter(formatter_file)
-
 logger = logging.getLogger(__name__)
 
-# logger.addHandler(file_handler)
-# logger.setLevel(logging.DEBUG)
+
+class Settings(BaseSettings):
+    POSTGRES_NAME: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    SQL_HOST: str
+    SQL_PORT: int
+    ELASTICSEARCH_HOST: str
+    ELASTICSEARCH_PORT: int
+    batch_size: int = 200
+    sleep_time: int = 10
+    tables: tuple[str, ...] = (
+        "film_work",
+        "genre",
+        "person",
+        "genre_film_work",
+        "person_film_work",
+    )
+    db_schema: dict[str, str] = {"postgres": "content."}
+
+
+class SettingsDocker(Settings):
+    class Config:
+        env_file = "doc.env"
+
+
+class SettingsDev(Settings):
+    class Config:
+        env_file = "dev.env"
+
+
+def get_settings() -> BaseSettings:
+    environment = os.environ.get("APP_ENV", "docker")
+    logger.info("\nОкружение: \n'%s'\n", environment)
+    if environment == "docker":
+        return SettingsDocker()
+    if environment == "dev":
+        return SettingsDev()
+    else:
+        os.environ["APP_ENV"] = "docker"
+        logger.warning(
+            "\nЗадано некорректное окружение: \n'%s'.\n"
+            "Присвоено значение по умолчанию: 'docker'\n",
+            environment,
+        )
+        return SettingsDocker()
+
+
+settings = get_settings()
+
+DSL = {
+    "dbname": settings.POSTGRES_NAME,
+    "user": settings.POSTGRES_USER,
+    "password": settings.POSTGRES_PASSWORD,
+    "host": settings.SQL_HOST,
+    "port": settings.SQL_PORT,
+}
+
+ES_HOST = settings.ELASTICSEARCH_HOST
+ES_PORT = settings.ELASTICSEARCH_PORT
+
+TABLES = settings.tables
+
+DB_SCHEMA = settings.db_schema
+
+BATCH_SIZE = settings.batch_size
+
+SLEEP_TIME = settings.sleep_time
